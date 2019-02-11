6b191b5c436780884d669679db7598cb556b6196
belamessex <belamessex@gmail.com>
2019-02-10 17:32:43 -0800

    Initial commit

---
+++ Source/PeachyPlayground/PeachyPlaygroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PeachyPlaygroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEACHYPLAYGROUND_API APeachyPlaygroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
